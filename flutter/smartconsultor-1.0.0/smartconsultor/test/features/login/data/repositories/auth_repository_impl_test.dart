1930f922dc3b580b45089f8c7898cd78fa315de4
tuanta2024@gmail.com <tuanta2024@gmail.com>
2023-12-29 19:32:02 +0700

    Flutter frontend 7

--- flutter/smartconsultor-1.0.0/smartconsultor/test/features/login/data/repositories/auth_repository_impl_test.dart
+++ flutter/smartconsultor-1.0.0/smartconsultor/test/features/login/data/repositories/auth_repository_impl_test.dart
@@ -2,21 +2,30 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:smartconsultor/core/network/network_info.dart';
+import 'package:smartconsultor/features/login/data/datasources/user_remote_data_source.dart';
+import 'package:smartconsultor/features/login/data/repositories/auth_repository_impl.dart';
 
-
+class MockUserRemoteDataSource extends Mock implements UserRemoteDataSource {}
 class MockConnectivity extends Mock implements Connectivity {}
 
 void main() {
-  MockConnectivity connectivity = MockConnectivity();
+  MockConnectivity mockConnectivity= MockConnectivity();
+  NetworkInfo networkInfo=NetworkInfo(mockConnectivity);  
+  MockUserRemoteDataSource mockUserRemoteDataSource=MockUserRemoteDataSource();
+  AuthRepositoryImpl authRepositoryImpl=AuthRepositoryImpl(remoteDataSource: mockUserRemoteDataSource, networkInfo: networkInfo);
 
   group('isConnected', () {
     test('should return true when ConnectivityResult is not none', () async {
       // Arrange
-      when(() => connectivity.checkConnectivity()).thenAnswer((_) async => ConnectivityResult.ethernet);
+      when(() => mockConnectivity.checkConnectivity()).thenAnswer((_) async => ConnectivityResult.ethernet);
       // Act
-      final result = await connectivity.checkConnectivity();
+      final result = await mockConnectivity.checkConnectivity();
       // Assert
       expect(result, ConnectivityResult.ethernet);
     });
   });
+
+  
+
 }
