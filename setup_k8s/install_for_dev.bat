b7199aeb71f1dd2c1c45162adafddd79607e217d
tuanta2024@gmail.com <tuanta2024@gmail.com>
2024-08-09 17:28:40 +0700

    09082024

--- setup_k8s/install_for_dev.bat
+++ setup_k8s/install_for_dev.bat
@@ -345,7 +345,7 @@ https://console.twilio.com/
 curl 'https://verify.twilio.com/v2/Services/VA70432cec771ac9af29ded3016f60fc34/Verifications' -X POST \
 --data-urlencode 'To=+84919358683' \
 --data-urlencode 'Channel=sms' \
--u AC8aff867bf9767657477aa2e12c8ab094:48be4f8ce469d7a75dce80dba36d15bc
+-u [user]:[token]
 {
   "status": "pending",
   "payee": null,
@@ -357,7 +357,7 @@ curl 'https://verify.twilio.com/v2/Services/VA70432cec771ac9af29ded3016f60fc34/V
       "time": "2024-08-08T03:14:09.522Z"
     }
   ],
-  "account_sid": "AC8aff867bf9767657477aa2e12c8ab094",
+  "account_sid": "[user]",
   "to": "+84919358683",
   "amount": null,
   "valid": false,
@@ -374,12 +374,12 @@ curl 'https://verify.twilio.com/v2/Services/VA70432cec771ac9af29ded3016f60fc34/V
 curl 'https://verify.twilio.com/v2/Services/VA2daad0858d96e2d514ca1a107c754b8e/VerificationCheck' -X POST \
 --data-urlencode 'To=+84919358683' \
 --data-urlencode 'Code=180312' \
--u AC8aff867bf9767657477aa2e12c8ab094:48be4f8ce469d7a75dce80dba36d15bc
+-u [user]:[token]
 {
   "status": "approved",
   "payee": null,
   "date_updated": "2024-08-08T03:15:26Z",
-  "account_sid": "AC8aff867bf9767657477aa2e12c8ab094",
+  "account_sid": "[user]",
   "to": "+84919358683",
   "amount": null,
   "valid": true,
